4c5a808b593017e8c93ae49af49eefa5f99a36c1
agropelli <angelica.gropelli@gmail.com>
2022-10-12 18:18:54 -0700

    fifty commit

--- ChessAngelica1/src/chess/ChessPiece.java
+++ ChessAngelica1/src/chess/ChessPiece.java
@@ -4,17 +4,20 @@ import boardgame.Piece;
 
 public class ChessPiece extends Piece{
     private Color color;
+    public ChessPiece(Board board, Color color){
+        super(board);
+        this.color = color;
+    }
 
-    
+    public Color getColor() {
+        return this.color;
+    }
+
+    p
     }
 
 
-
     
-    }
-    
-
-
 
     
 }
