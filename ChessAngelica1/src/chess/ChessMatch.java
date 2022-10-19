d13e9f1f52f74af567da4bac9d50a46d48b2acb6
agropelli <angelica.gropelli@gmail.com>
2022-10-19 15:29:04 -0700

    Possible moves of a piece

--- ChessAngelica1/src/chess/ChessMatch.java
+++ ChessAngelica1/src/chess/ChessMatch.java
@@ -33,15 +33,14 @@ public class ChessMatch {
         Piece capturedPiece = board.removePiece(target);
         board.placePiece (p,target);
         return capturedPiece;
-        
     }
-
-
-
 
     private void validateSourcePosition(Position position){
         if(!board.thereIsAPiece(position)){
             throw new ChessException("There is no piece on source position");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("There is not possible moves for the chosen piece");
         }
     }
 
