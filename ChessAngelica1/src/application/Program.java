997c9e4e70b9343fb7385ae2eb3a76b75ce8ae91
agropelli <angelica.gropelli@gmail.com>
2022-10-19 14:13:05 -0700

    cleaning screen

--- ChessAngelica1/src/application/Program.java
+++ ChessAngelica1/src/application/Program.java
@@ -1,5 +1,7 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -10,18 +12,27 @@ public class Program {
       ChessMatch chessMatch = new ChessMatch();
       
       while (true){
+         try{
+            UI.clearScreen();
+            UI.printBoard(chessMatch.getPieces()); 
+            System.out.println();
+            System.out.print("Source:  ");
+            ChessPosition source = UI.readChessPosition(sc);
 
-         UI.printBoard(chessMatch.getPieces()); 
-         System.out.println();
-         System.out.print("Source:  ");
-         ChessPosition source = UI.readChessPosition(sc);
-
-         System.out.println();
-         System.out.print("Target:   ");
-         ChessPosition target = UI.readChessPosition(sc);
+            System.out.println();
+            System.out.print("Target:   ");
+            ChessPosition target = UI.readChessPosition(sc);
          
-         ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
-         
+            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+         }
+         catch (ChessException e){
+            System.out.println(e.getMessage());
+            sc.nextLine();
+         }
+         catch (InputMismatchException e){
+            System.out.println(e.getMessage());
+            sc.nextLine();
+         }
       }  
    } 
 }
