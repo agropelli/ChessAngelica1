f5c78622d45bf2163e839b5bf7c3258bfab55241
agropelli <angelica.gropelli@gmail.com>
2022-11-03 16:57:20 -0700

    Queen

--- ChessAngelica1/src/chess/ChessMatch.java
+++ ChessAngelica1/src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import piecesofchess.Bishop;
 import piecesofchess.King;
 import piecesofchess.Knight;
 import piecesofchess.Pawn;
+import piecesofchess.Queen;
 import piecesofchess.Rook;
 
 public class ChessMatch {
@@ -196,6 +197,7 @@ public class ChessMatch {
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -212,6 +214,7 @@ public class ChessMatch {
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
